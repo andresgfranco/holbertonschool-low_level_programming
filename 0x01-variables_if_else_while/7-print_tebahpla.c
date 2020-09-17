15c8a13c566bff456e28ec118ebbb5a587f1d532
andresgfranco <andresgfranco0@gmail.com>
2020-09-17 23:26:51 +0000

    Adding program number 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase in reverse
+ *
+ * Return: 0
+ **/
+int main(void)
+{
+	int c;
+
+	for (c = 122; c >= 97; c--)
+	{
+	putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
