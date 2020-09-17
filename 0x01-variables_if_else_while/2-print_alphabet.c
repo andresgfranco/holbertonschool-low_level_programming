bbfc89e7a65f224ee447e945d604e7ca61dc0c5e
andresgfranco <andresgfranco0@gmail.com>
2020-09-17 21:34:34 +0000

    Adding program printing the alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+  * main - prints the alphabet in lowercase
+  *
+  * Return: 0
+**/
+int main(void)
+{
+	int c;
+
+	for (c = 97; c <= 122; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
