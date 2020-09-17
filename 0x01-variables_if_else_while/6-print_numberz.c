946e201d8318227d9a48ed8a9eced0d3a5570fdd
andresgfranco <andresgfranco0@gmail.com>
2020-09-17 23:23:31 +0000

    Adding program number 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints all single digit numbers of base 10 using putchar
+ *
+ * Return: 0
+**/
+int main(void)
+{
+	int c;
+
+	for (c = 48; c <= 57; c++)
+	{
+	putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
