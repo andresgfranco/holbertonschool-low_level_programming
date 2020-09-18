ce04516e7459eb484ddfe03d0dcc5b79d9ee0145
andresgfranco <andresgfranco0@gmail.com>
2020-09-18 00:14:24 +0000

    Adding program number 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - prints all the numbers of base 16 
+ *
+ * Return: 0
+ **/
+int main(void)
+{
+	int c;
+
+	for (c = 48; c <= 57; c++)
+	{
+	putchar(c);
+	}
+	for (c = 97; c <= 102; c++)
+	{
+	putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
