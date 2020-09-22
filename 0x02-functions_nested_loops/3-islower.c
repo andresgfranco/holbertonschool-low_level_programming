6aeebe76294ceebbb514b16f23cfd09f63b58430
andresgfranco <andresgfranco0@gmail.com>
2020-09-22 17:46:40 +0000

    Correction on betty style

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,14 +2,15 @@
 /**
   * _islower - checks for lowercase character
   *
+  * @c: parameter character in ascii code
   * Return: 1 if c is lowercase or 0 otherwise
 **/
 int _islower(int c)
 {
 
 	if (c >= 97 && c <= 122)
-		return(1);
+		return (1);
 	else
-		return(0);
+		return (0);
 
 }
