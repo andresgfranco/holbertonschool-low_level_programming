0574d9b0155747cc12b1efd9e039ce17b152c908
andresgfranco <andresgfranco0@gmail.com>
2020-09-22 05:44:03 +0000

    Adding first program

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+  * print_alphabet - prints the alphabet in lowercase
+  *
+  * Return: void
+**/
+void print_alphabet (void)
+{
+	int c;
+
+	for(c = 97; c <= 122; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+
+}
