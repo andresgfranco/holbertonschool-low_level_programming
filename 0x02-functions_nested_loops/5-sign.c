f0af9cf5c9b19bf9cb0cb33755c7c3be27b48250
andresgfranco <andresgfranco0@gmail.com>
2020-09-22 18:09:24 +0000

    Adding program number 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+  * print_sign - prints the sign of a number
+  *
+  * @n: number in ascii code
+  * Return: 1 if is greater than zero, 0 if is zero and -1 if is les than zero
+**/
+int print_sign(int n)
+{
+
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n > 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+		_putchar(48);
+		return (0);
+
+}
