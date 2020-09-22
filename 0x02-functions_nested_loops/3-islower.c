b052cc7c37057398c305d489bc078847e1a867c1
andresgfranco <andresgfranco0@gmail.com>
2020-09-22 17:43:13 +0000

    Adding program number 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+  * _islower - checks for lowercase character
+  *
+  * Return: 1 if c is lowercase or 0 otherwise
+**/
+int _islower(int c)
+{
+
+	if (c >= 97 && c <= 122)
+		return(1);
+	else
+		return(0);
+
+}
