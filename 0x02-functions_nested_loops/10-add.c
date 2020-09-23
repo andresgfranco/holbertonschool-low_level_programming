9299b5e98daa4415f670e034fdd4ae72095bf4d9
andresgfranco <andresgfranco0@gmail.com>
2020-09-23 00:19:21 +0000

    Adding program number 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+  * add - adds two integers and returns the result
+  * @x: integer to be added
+  * @y: integer to be added
+  * Return: the result
+**/
+int add(int x, int y)
+{
+	int z;
+
+	z = x + y;
+
+	return (z);
+
+}
