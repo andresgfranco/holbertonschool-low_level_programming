12969e06f00457a5b3dc9ceed47b58fc5c1bf735
andresgfranco <andresgfranco0@gmail.com>
2020-09-24 17:14:33 +0000

    Adding program number 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+  * _isdigit - checks for a digit
+  *
+  * @c: integer
+  * Return: 1 if c is uppercase or 0 otherwise
+**/
+int _isdigit(int c)
+{
+
+	if (c >= 48 && c <= 57)
+		return (1);
+	return (0);
+
+}
