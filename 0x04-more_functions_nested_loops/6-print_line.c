946becbebb279d119275e5b14139fccbd3bb6f1c
andresgfranco <andresgfranco0@gmail.com>
2020-09-25 04:51:08 +0000

    Adding program number 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+  * print_line - draws a straight line in the terminal
+  * @n: lenght of line
+  * Return: void
+**/
+void print_line(int n)
+{
+	int l;
+
+	for (l = 1; l <= n; l++)
+		_putchar('_');
+
+}
