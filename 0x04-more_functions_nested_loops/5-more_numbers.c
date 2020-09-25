b5ec6a4549c60f803deee1c328f00b2862128504
andresgfranco <andresgfranco0@gmail.com>
2020-09-25 04:35:57 +0000

    Adding program number 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+  * more_numbers - prints 10 times the numbers, from 0 to 14
+  *
+  * Return: void
+**/
+void more_numbers(void)
+{
+
+	int l;
+	int n;
+			     
+	for (l = 0; l <= 10; l++)
+	{
+		for (n = 0; n <= 14; n++)
+		{
+			if (n > 9)
+				putchar (49);
+			putchar (n % 10 + '0');
+		}
+		putchar ('\n');
+	}
+
+return 0;
+}
