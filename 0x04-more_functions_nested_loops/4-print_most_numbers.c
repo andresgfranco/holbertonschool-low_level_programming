753690b4ba1935acbb832008491bd9da7b77ee3b
andresgfranco <andresgfranco0@gmail.com>
2020-09-25 03:51:48 +0000

    Correction on the function name

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,10 +1,10 @@
 #include "holberton.h"
 /**
-  * print_numbers - prints the numbers, from 0 to 9
+  * print_most_numbers - prints some of the numbers, from 0 to 9
   *
   * Return: void
 **/
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	int n;
 
