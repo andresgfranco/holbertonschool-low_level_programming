fef2849e2fe6ac10d2d9b78a5bfbb416b51fe355
andresgfranco <andresgfranco0@gmail.com>
2020-09-25 04:57:14 +0000

    Correction on requirements

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,7 +8,14 @@ void print_line(int n)
 {
 	int l;
 
-	for (l = 1; l <= n; l++)
-		_putchar('_');
-
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (l = 1; l <= n; l++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
 }
