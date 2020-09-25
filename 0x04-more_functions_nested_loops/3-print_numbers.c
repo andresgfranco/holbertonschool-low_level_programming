f8dafad48e5d3221375a860bb04b175dc3ca485c
andresgfranco <andresgfranco0@gmail.com>
2020-09-25 03:43:19 +0000

    Adding third function

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+  * print_numbers - prints the numbers, from 0 to 9
+  *
+  * Return: void
+**/
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 48; n <= 58; n++)
+	{
+		_putchar (n);
+	}
+	_putchar ('\n');
+}
