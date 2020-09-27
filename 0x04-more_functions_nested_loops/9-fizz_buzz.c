4a408ac035a23cf9c62598dbf1725da69e4e3973
andresgfranco <andresgfranco0@gmail.com>
2020-09-27 04:20:29 +0000

    Fun FizzBuzz algorithm

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+  * main - FizzBuzz algorithm
+  *
+  * Return: 0 if success
+**/
+int main(void)
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+		if (n % 5 == 0 && n % 3 == 0)
+			printf("FizzBuzz ");
+		else if (n % 5 == 0)
+			printf("Buzz ");
+		else if (n % 3 == 0)
+			printf("Fizz ");
+		else
+		printf("%d ", n);
+	}
+	putchar('\n');
+return (0);
+}
