a4f08de715a911d0543a0efaac4ae465d9ebc24f
andresgfranco <andresgfranco0@gmail.com>
2020-09-29 23:57:50 +0000

    Adding program number 6 and header

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+  * puts2 - prints every other character of a string, starting with the first
+  * @str: string coming
+  * Return: void
+**/
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
