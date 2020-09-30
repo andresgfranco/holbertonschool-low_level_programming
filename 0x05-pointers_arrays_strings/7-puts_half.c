4996fdb8635b7dc0a94c002a9bb3352aeeacff68
andresgfranco <andresgfranco0@gmail.com>
2020-09-30 01:45:40 +0000

    Adding program number 7 and header

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+  * puts_half - prints half of a string
+  * @str: string
+  * Return: void
+**/
+void puts_half(char *str)
+{
+	int l;
+
+	for (l = 0; str[l] != '\0'; l++)
+	{
+	}
+	l--;
+	if ((l % 2) == 0)
+	{
+		l /= 2;
+	}
+	else
+	{
+		l = ((l + 1) / 2);
+	}
+	for (; str[l] != '\0'; l++)
+	{
+
+		_putchar(str[l]);
+	}
+	_putchar('\n');
+}
