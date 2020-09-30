014baf35a86ff509fab7011aed811ae8442da480
andresgfranco <andresgfranco0@gmail.com>
2020-09-30 03:35:50 +0000

    Adding program number 9 and header

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+  * _strcpy - copies the string pointed to by src, to the buffer
+  * @dest: destination
+  * @src: source
+  * Return: the pointer to dest
+**/
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	return (dest);
+}
