a3845e4dfded26d738339a78c94de805c6d5c5ad
andresgfranco <andresgfranco0@gmail.com>
2020-10-01 21:22:18 +0000

    Adding function number 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+  * _strncat - Concatenates two strings
+  * @dest: destination string
+  * @src: source string
+  * @n: number of characters
+  * Return: a pointer to the resulting string dest
+**/
+char *_strncat(char *dest, char *src, int n)
+{
+	int s1, s2;
+	
+	for (s1 = 0; dest[s1] != '\0'; s1++)
+	{
+	}
+	for (s2 = 0; s2 < n; s2++)
+	{
+		dest[s1] = src[s2];
+		s1++;
+	}
+	
+	return (dest);
+}
