7ff047618e0d305676bb46850ffadde2380a05b1
andresgfranco <andresgfranco0@gmail.com>
2020-10-01 20:16:34 +0000

    Made it more simplified

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,17 +7,14 @@
 **/
 char *_strcat(char *dest, char *src)
 {
-	int s1, s2, i;
+	int s1, s2;
 
 	for (s1 = 0; dest[s1] != '\0'; s1++)
 	{
 	}
 	for (s2 = 0; src[s2] != '\0'; s2++)
 	{
-	}
-	for (i = 0; i <= s2; i++)
-	{
-		dest[s1] = src[i];
+		dest[s1] = src[s2];
 		s1++;
 	}
 	return (dest);
