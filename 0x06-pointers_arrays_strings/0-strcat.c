5ea396a0c59644f97b8223a209fe3afdc845c74f
andresgfranco <andresgfranco0@gmail.com>
2020-10-01 18:58:02 +0000

    Correction on betty and variables

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,18 +9,18 @@ char *_strcat(char *dest, char *src)
 {
 	int s1, s2, s3, i;
 
-	for(s1 = 0; dest[s1] != '\0'; s1++)
+	for (s1 = 0; dest[s1] != '\0'; s1++)
 	{
 	}
-	for(s2 = 0; src[s2] != '\0'; s2++)
+	for (s2 = 0; src[s2] != '\0'; s2++)
 	{
 	}
-	s2--;
 	s3 = s1 + s2;
-	for(i = 0; s2 <= s3; s2++)
+	for (i = 0; s2 <= s3; s2++)
 	{
 		dest[s2] = src[i];
 		i++;
 	}
+	dest[s2] = src[s2];
 	return (dest);
 }
