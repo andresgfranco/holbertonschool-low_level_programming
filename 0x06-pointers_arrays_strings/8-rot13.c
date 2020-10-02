742c177984866f1a8cea454359074224d1661f50
andresgfranco <andresgfranco0@gmail.com>
2020-10-02 02:52:45 +0000

    Adding program number 8 and header

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+  * rot13 - encodes a string using rot13
+  * @s: string
+  * Return: pointer to s
+**/
+char *rot13(char *s)
+{
+	int i, j;
+	char regular[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for(i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; regular[j] != '\0'; j++)
+		{
+			if (s[i] == regular[j])
+			{
+				s[i] = rot[j];
+			}
+		}
+		j = 0;
+	}
+	return (s);
+}
