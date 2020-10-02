1a52589c311606e0ea1635e0f75878d3d44f9d1e
andresgfranco <andresgfranco0@gmail.com>
2020-10-02 00:58:50 +0000

    Adding third program and header

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+  * _strcmp - compares two strings
+  * @s1: string 1
+  * @s2: string 2
+  * Return: integer 
+**/
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] == s2[i]; i++)
+		if (s1[i] == '\0')
+			return (0);
+	return (s1[i] - s2[i]);
+}
