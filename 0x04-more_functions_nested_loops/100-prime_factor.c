41d2f9574618b950a9fd57ad88ad418cfbaf3f3f
andresgfranco <andresgfranco0@gmail.com>
2020-10-03 05:31:12 +0000

    Adding advanced number 11

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -6,9 +6,20 @@
 **/
 int main(void)
 {
-	long long int n = 612852475143 / 3;
+	long int n = 612852475143;
+	long int x;
+	long int p;
 
-	printf("%lld\n", n);
+	for (x = 2; x <= n; x++)
+	{
+		if ((n % x) == 0)
+		{
+			p = x;
+			n /= x;
+			x = 1;
+		}
+	}
+	printf("%ld\n", p);
 
 	return (0);
 }
