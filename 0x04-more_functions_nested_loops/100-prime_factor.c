629071cac0c77de4ff8f991c305c61f5345afb91
andresgfranco <andresgfranco0@gmail.com>
2020-10-03 03:36:56 +0000

    Adding advanced number 11

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+  * main - finds and prints the largest prime factor of the number 612852475143
+  *
+  * Return: 0 if success
+**/
+int main(void)
+{
+	long long int n = 612852475143 / 3;
+
+	printf("%lld\n", n);
+}
