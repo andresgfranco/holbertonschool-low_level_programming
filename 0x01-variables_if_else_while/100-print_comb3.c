d55f2d68e3da027224fe1e74c22f5c7e33aba1b8
andresgfranco <andresgfranco0@gmail.com>
2020-10-03 02:49:20 +0000

    Adding advanced number 11

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h> 
+/**
+  * main - prints all possible different combinations of two digits
+  * 
+  * Return: 0 if success
+**/
+int main(void)
+{
+
+	int x, n1, n2;
+	  
+	for (x = 0; x < 100; x++)
+	{
+		n1 = x / 10;
+		n2 = x % 10;
+		if (n2 > n1)
+		{
+			if (x > 1)
+			{
+				putchar (',');
+				putchar (' ');
+			}
+			putchar (n1 + 48);
+			putchar (n2 + 48);
+		}
+	}
+	putchar ('\n');
+	return (0);
+}
