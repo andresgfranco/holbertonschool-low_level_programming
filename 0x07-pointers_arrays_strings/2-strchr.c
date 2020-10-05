319aba82137dfac1149dcea10bce4c909fc4c128
andresgfranco <andresgfranco0@gmail.com>
2020-10-05 20:58:38 +0000

    Adding new conditional

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,14 +8,16 @@
 char *_strchr(char *s, char c)
 {
 	int i;
-	
-	if (s == NULL)
-		return (NULL);
-	for (i = 0; s[i] != '\0'; i++)
+
+	for (i = 0; s[i] == '\0'; i++)
 	{
 		if (s[i] == c)
 		{
-			return(&s[i]);
+			return (&s[i]);
+		}
+		else if (s[i] == '\0')
+		{
+			return (NULL);
 		}
 	}
 
