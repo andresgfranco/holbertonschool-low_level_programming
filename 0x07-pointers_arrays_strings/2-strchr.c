04d73a349fe69b6b65225d18f99eaf79e1e5a3b8
andresgfranco <andresgfranco0@gmail.com>
2020-10-05 21:52:08 +0000

    Change conditionals

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,17 +9,15 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	for (i = 0; s[i]; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == c)
 		{
-			return (&s[i]);
-		}
-		if (s[i] == '\0')
-		{
-			return (NULL);
+			break;
 		}
 	}
+	if (s[i] == c)
+		return(&s[i]);
 
 	return (NULL);
 }
