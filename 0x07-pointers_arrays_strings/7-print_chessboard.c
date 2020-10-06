159665f9b3e001612ff451d371865ad407715109
andresgfranco <andresgfranco0@gmail.com>
2020-10-06 04:54:45 +0000

    Adding function number 7 and header

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * print_chessboard - prints the chessboard
+ * @a: is the array of the chessboard
+ * Return: void
+**/
+void print_chessboard(char (*a)[8])
+{
+	int i = 0, b = 0;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (b = 0; b < 8; b++)
+			_putchar(a[i][b]);
+		_putchar('\n');
+	}
+}
