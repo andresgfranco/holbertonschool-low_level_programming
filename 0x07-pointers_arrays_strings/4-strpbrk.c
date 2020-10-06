450271555fc5dd9974a63dbc23295f72721e9d8c
andresgfranco <andresgfranco0@gmail.com>
2020-10-06 00:52:43 +0000

    Adding function number 4 and header

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+  * _strpbrk - seaches a string for any of a set of bytes
+  * @s: string entering
+  * @accept: string of reference
+  * Return: pointer to s or NULL if no such byte is found
+**/
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				break;
+			}
+		}
+		if (s[i] == accept[j])
+			break;
+	}
+
+	return (&s[i]);
+}
