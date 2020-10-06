d6da72e57815adcde3785c4d71ccde88027891e0
andresgfranco <andresgfranco0@gmail.com>
2020-10-06 04:59:51 +0000

    Adding function number 9 and header

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * set_string - sets de value of a pointer to a char
+ * @s: Double pointer
+ * @to: Simple pointer
+ * Return: void
+**/
+void set_string(char **s, char *to)
+{
+		*s = to;
+}
