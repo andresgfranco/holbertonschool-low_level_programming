673a8e725ff6c2664a99cf2abfde52c08f1b9bac
andresgfranco <andresgfranco0@gmail.com>
2020-10-07 18:42:30 +0000

    Correction on Betty style

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,12 +1,13 @@
 #include "holberton.h"
 /**
-  * _puts_recursion(char *s)
+  * _puts_recursion(char *s) - prints a string
   * @s: string coming
   * Return: 0 when string is finished
 **/
 void _puts_recursion(char *s)
 {
 	int i = 0;
+
 	if (s[i] == '\0')
 	{
 		_putchar(10);
@@ -14,6 +15,5 @@ void _puts_recursion(char *s)
 	}
 	_putchar (*s);
 	s++;
-
 	_puts_recursion(s);
 }
