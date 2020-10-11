1993b32ac5d5927b70c1ac34120da55db06497c0
andresgfranco <andresgfranco0@gmail.com>
2020-10-11 22:13:38 +0000

    Adding corresponding files

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+  * main - print its name
+  * @argc - the number of command line arguments
+  * @argv - array containint the program command line arguments
+  * Return: 0
+**/
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc - 1]);
+	return (0);
+}
