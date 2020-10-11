473ef7297aa3aae2b40298a4cfa168d95e237585
andresgfranco <andresgfranco0@gmail.com>
2020-10-11 22:17:21 +0000

    Adding function number 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+  * main - prints the number of arguments passed into it
+  * @argc: the number of command line arguments
+  * @argv: is an array containing the program command line arguments
+  * Return: 0
+**/
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
