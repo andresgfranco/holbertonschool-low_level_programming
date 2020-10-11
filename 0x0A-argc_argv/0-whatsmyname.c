26320e9baee9c5ca67e08536570a36dbc810d35a
andresgfranco <andresgfranco0@gmail.com>
2020-10-11 22:14:55 +0000

    Adding corresponding file

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,8 +1,8 @@
 #include "holberton.h"
 /**
   * main - print its name
-  * @argc - the number of command line arguments
-  * @argv - array containint the program command line arguments
+  * @argc: the number of command line arguments
+  * @argv: array containint the program command line arguments
   * Return: 0
 **/
 int main(int argc, char *argv[])
