3d889cdaab407c2882086d321ef0e797c8c7930a
andresgfranco <andresgfranco0@gmail.com>
2020-10-14 03:12:16 +0000

    Adding readme, header and function number 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+  * create_array - creates an array of chars, and initializes it
+  * with a specific char.
+  * @size: size of the string
+  * @c: specific char
+  * Return: NULL if size 0 or malloc fails
+  * or pointer to the array if success
+**/
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *str;
+
+	str = malloc(size * (sizeof(char)));
+
+	if (str == NULL)
+		return (NULL);
+	if (size == 0)
+		return (NULL);
+	for (i = 0; i < size; i++)
+	{
+		str[i] = c;
+	}
+	str[i] = '\0';
+	return (str);	
+}
