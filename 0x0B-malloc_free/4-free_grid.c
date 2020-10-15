cb80dd42414ab62a91091e8462355a40fa2ea0e9
andresgfranco <andresgfranco0@gmail.com>
2020-10-15 18:39:37 +0000

    Adding function number 4 and header

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+  * free_grid - frees a 2 dimensional grid previously created
+  * @grid: width
+  * @height: height of the 2 dimensional grid
+  * Return: void
+**/
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free (grid[i]);
+	}
+	free(grid);
+}
