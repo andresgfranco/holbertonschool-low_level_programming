df912af6453a93144753067c5efa7f78162fee71
andresgfranco <andresgfranco0@gmail.com>
2020-10-19 19:07:41 +0000

    Corections on comments of struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #ifndef DOG_H_
 #define DOG_H_
 
+/**
+  * struct dog - data type to save dogs basic data
+  * @name: dog's name
+  * @age: dog's age
+  * @owner: dog's owner
+**/
 struct dog {
 	char *name;
 	float age;
