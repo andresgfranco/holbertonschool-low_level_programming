2410df42d4de4b5279e0ceaeeb137f4520c61d1e
andresgfranco <andresgfranco0@gmail.com>
2020-10-19 18:09:58 +0000

    Adding function number 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+  * main - prints the name of the file it was complied from
+  *
+  * Return: 0 if sucess
+**/
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
