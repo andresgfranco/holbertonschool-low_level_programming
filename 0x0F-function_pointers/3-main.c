440298b3130fa75c7c285d0d09442039160d9b56
andresgfranco <andresgfranco0@gmail.com>
2020-10-21 22:12:18 +0000

    Correction on parentheses

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -12,12 +12,12 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-	if (*argv[2] != '+' &&
+	if ((*argv[2] != '+' &&
 				*argv[2] != '-' &&
 				*argv[2] != '*' &&
 				*argv[2] != '/' &&
-				*argv[2] != '%' ||
-				*argv[2][1] != 0
+				*argv[2] != '%') ||
+				argv[2][1] != 0
 				)
 	{
 		printf("Error\n");
