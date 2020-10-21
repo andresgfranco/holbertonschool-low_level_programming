dfd592ca8e041778ff66c9097823699b7d689f2a
andresgfranco <andresgfranco0@gmail.com>
2020-10-21 17:50:48 +0000

    Function number 1 and header

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+/**
+  * array_iterator - executes a function given as a parameter
+  * on each element of an array
+  * @array: array coming
+  * @size: size of array
+  * @action: function to execute
+  * Return: void
+**/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
