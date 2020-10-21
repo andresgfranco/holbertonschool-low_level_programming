62b8e2729d37b09e48e429096540788b4055704a
andresgfranco <andresgfranco0@gmail.com>
2020-10-21 21:13:38 +0000

    Correction on Betty

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -17,9 +17,7 @@ typedef struct op
 	int (*f)(int a, int b);
 } op_t;
 
-/**
-  function prototypes
-**/
+/* function prototypes */
 
 int op_add(int a, int b);
 int op_sub(int a, int b);
@@ -27,10 +25,7 @@ int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
 
-/**
-  * function prototype that will
-  * determine which function will call
-**/
+/*  function prototype that will  determine which function will call */
 int (*get_op_func(char *s))(int, int);
 
 #endif
