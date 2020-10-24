7ebf2b27fe351ed4e677bdff6e7f9dcb1ef1acd0
andresgfranco <andresgfranco0@gmail.com>
2020-10-24 18:59:37 +0000

    Correction on extra conditional

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -10,13 +10,11 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list valist;
 
-	if (n == 0)
-		return;
 	if (separator == NULL)
 		separator = "";
 
 	va_start(valist, n);
-	
+
 	for (i = 0; i < n; i++)
 	{
 		if (i == (n - 1))
