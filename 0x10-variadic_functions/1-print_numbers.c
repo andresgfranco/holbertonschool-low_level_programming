7fa4180ec62e3085e89c5ed88b96f384882543fc
andresgfranco <andresgfranco0@gmail.com>
2020-10-24 18:55:39 +0000

    Adding readme, header and first two functions

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+/**
+  * print_numbers - prints numbers, followed by a new line
+  * @separator: , and whitespace
+  * @n: number of integers passed to the function
+  * Return: void
+**/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list valist;
+
+	if (n == 0)
+		return;
+	va_start(valist, n);
+
+	for (i = 0; i < n; i++)
+	{
+		if (i == (n - 1))
+			printf("%d", va_arg(valist, int));
+		else
+			printf("%d%s", va_arg(valist, int), separator);
+	}
+	printf("\n");
+
+	va_end(valist);
+}
