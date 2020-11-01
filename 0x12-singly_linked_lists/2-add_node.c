dbf08f2117273834085651214618b34e033ad8e8
andresgfranco <andresgfranco0@gmail.com>
2020-11-01 19:40:12 +0000

    Correction on requirements

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+unsigned int _strlen(const char *s);
 /**
   * add_node - adds a new node at the beginning of a list
   * @head: first element of list
@@ -9,17 +10,31 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_head;
-	char *str_duplicate = strdup(str);
 
 	new_head = (list_t *) malloc(sizeof(list_t));
 
 	if (new_head == NULL)
 		return (0);
 
-	new_head->str = str_duplicate;
-	new_head->len = strlen(str_duplicate);
+	new_head->str = strdup(str);
+	new_head->len = _strlen(str);
 	new_head->next = (*head);
 	(*head) = new_head;
 
 	return (new_head);
+}
+/**
+    * _strlen - returns the lenght of a string
+      * @s: string coming
+        * Return: lenght of a string
+	**/
+unsigned int _strlen(const char *s)
+{
+	unsigned int l;
+
+	for (l = 0; s[l] != '\0'; l++)
+	{
+	}
+	return (l);
+
 }
