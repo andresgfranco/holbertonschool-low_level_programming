add05352f87a0fd8f457f65624049e127d3cfc3e
andresgfranco <andresgfranco0@gmail.com>
2020-11-04 23:19:20 +0000

    Adding last conditional

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,6 +11,10 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	if (head == NULL)
 		return (NULL);
+
+	if (index == 0)
+		return (head);
+
 	for (i = 0; i < index; i++)
 		head = head->next;
 
