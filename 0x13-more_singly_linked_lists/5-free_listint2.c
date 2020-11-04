f24431be39e3fcdcd778db5e7a3bfb4a80f4ca89
andresgfranco <andresgfranco0@gmail.com>
2020-11-04 20:38:31 +0000

    Adding function number 5 and header

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+  * free_listint2 - frees a list
+  * @head: list to be freed
+  * Return: void
+**/
+void free_listint2(listint_t **head)
+{
+	listint_t *current, *next_node;
+
+	current = *head;
+
+	while (current != NULL)
+	{
+		next_node = current->next;
+		free(current);
+		current = next_node;
+	}
+	(*head) = NULL;
+}
