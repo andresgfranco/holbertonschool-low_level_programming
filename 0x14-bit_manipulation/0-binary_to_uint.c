a4c4b662fba45d0a2a6d6db30fa6fac931d524ab
andresgfranco <andresgfranco0@gmail.com>
2020-11-07 17:56:50 +0000

    Correction on Betty style

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,7 +2,7 @@
 /**
   * binary_to_uint - converts a binary number to unsigned int
   * @b: string of 0 and 1 chars
-  * Return: converted number of 0 if there is 
+  * Return: converted number of 0 if there is
   * one or more chars in the string b that is not 0 or 1
   * or b is NULL
 **/
@@ -10,6 +10,7 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int converted_n = 0, p = 0;
 	int i = 0;
+
 	if (b == NULL)
 		return (0);
 
@@ -27,12 +28,17 @@ unsigned int binary_to_uint(const char *b)
 	return (converted_n);
 }
 
+/**
+ * _pow_recursion - value of x raised to the power of y
+ * @x: integer
+ * @y: power
+ * Return: int
+**/
 int _pow_recursion(int x, int y)
 {
-	    if (y < 0)
-		    return (-1);
-    	    else if (y == 0)
-		    return (1);
-
-	    return (x * _pow_recursion(x, y - 1));
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
 }
