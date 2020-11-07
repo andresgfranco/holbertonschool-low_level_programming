2eae3e9e45b2e941d206672ecbe213a00d9ff66a
andresgfranco <andresgfranco0@gmail.com>
2020-11-07 17:54:14 +0000

    Adding first function, readme and header

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+/**
+  * binary_to_uint - converts a binary number to unsigned int
+  * @b: string of 0 and 1 chars
+  * Return: converted number of 0 if there is 
+  * one or more chars in the string b that is not 0 or 1
+  * or b is NULL
+**/
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int converted_n = 0, p = 0;
+	int i = 0;
+	if (b == NULL)
+		return (0);
+
+	for (; b[i] != '\0'; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+	}
+	for (i = i - 1; i != 0; i--)
+	{
+		converted_n += (b[i] - '0') * _pow_recursion(2, p);
+		p++;
+	}
+	converted_n += (b[i] - '0') * _pow_recursion(2, p);
+	return (converted_n);
+}
+
+int _pow_recursion(int x, int y)
+{
+	    if (y < 0)
+		    return (-1);
+    	    else if (y == 0)
+		    return (1);
+
+	    return (x * _pow_recursion(x, y - 1));
+}
---
+++ 0x14-bit_manipulation/holberton.h
@@ -0,0 +1,9 @@
+#ifndef HOLBERTON_H_
+#define HOLBERTON_H_
+
+#include <stdlib.h>
+
+unsigned int binary_to_uint(const char *b);
+int _pow_recursion(int x, int y);
+
+#endif
