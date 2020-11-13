1e4b14578ea3d49f84760acdf3e17420e89e5963
andresgfranco <andresgfranco0@gmail.com>
2020-11-13 23:45:12 +0000

    Adding first function, header and readme

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+/**
+  * read_textfile - reads a text file and prints it to
+  * the POSIX standart output
+  * @filename: name of file
+  * @letters: number of letters
+  * Return: actual number of letters it could read and print
+  * if the file can not be opened or read, return 0
+  * if filename is NULL return 0
+**/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	char *buf;
+	ssize_t num_bytes, bytes_printed;
+
+	if (filename == NULL || letters == 0)
+		return (0);
+
+	buf = malloc(sizeof(char));
+	if (buf == NULL)
+		return (0);
+
+	buffer[letters] = '\0';
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	num_bytes = read(fd, buf, letters);
+
+	bytes_printed = write(STDOUT_FILENO, buf, num_bytes);
+
+	close(fd);
+	free(buffer);
+	return (numletters);
+}
