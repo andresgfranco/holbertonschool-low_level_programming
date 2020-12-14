5a014a46da066e37cb91b9925a024c6fa4a99782
andresgfranco <andresgfranco0@gmail.com>
2020-12-14 14:30:29 -0500

    Simplified conditional and added a new conditional when index is the last one

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,19 +11,13 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int i = 0;
 	dlistint_t *new_node = malloc(sizeof(dlistint_t)), *current_node = *h;
 
-	if (!new_node)
+	if ((!new_node) || (*h == NULL && idx > 0))
 	{
 		free(new_node);
 		return (NULL);
 	}
-
 	new_node->n = n;
 	new_node->next = NULL;
-	if (*h == NULL && idx > 0)
-	{
-		free(new_node);
-		return (NULL);
-	}
 	if (idx == 0)
 	{
 		new_node->next = *h;
@@ -45,6 +39,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	current_node->next = new_node;
 	current_node = current_node->next;
 	current_node = current_node->next;
-	current_node->prev = new_node;
+	if(current_node != NULL)
+		current_node->prev = new_node;
 	return (new_node);
 }
