7cbd51dd92fbb0c00f477d11b4d6325ad6bca380
andresgfranco <andresgfranco0@gmail.com>
2020-12-14 15:02:16 -0500

    Deleted a statement when index is equal to 0

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -23,7 +23,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		new_node->next = *h;
 		new_node->prev = NULL;
 		current_node->prev = new_node;
-		*h = new_node;
 		return (new_node);
 	}
 	for (; i < idx - 1; i++)
