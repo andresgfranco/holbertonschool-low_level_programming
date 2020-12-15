52aa17f665ea75669d2fe9ce0b84f12e3cbcaacc
andresgfranco <andresgfranco0@gmail.com>
2020-12-14 21:50:22 -0500

    Conditional

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -41,5 +41,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	current_node = current_node->next;
 	current_node = current_node->next;
 	current_node->prev = new_node;
+	if (current_node != NULL)
+	{
+		current_node->prev = new_node;
+	}
 	return (new_node);
 }
