f76e7073f5f89fb9a04b893f45d3ed37aaa3408d
andresgfranco <andresgfranco0@gmail.com>
2021-01-15 22:46:56 +0000

    Simplified code

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -9,9 +9,8 @@
 **/
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	unsigned long int key_index = 0;
+	unsigned long int key_index = hash_djb2(key);
 
-	key_index = hash_djb2(key);
 	key_index %= size;
 
 	return (key_index);
