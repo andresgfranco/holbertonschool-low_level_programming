24ebc99da905bb1b3adb97d1330eeb4f5bc88c48
andresgfranco <andresgfranco0@gmail.com>
2021-01-15 20:33:00 +0000

    Switch expression assignment of position

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -17,13 +17,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 			return (NULL);
 		}
 
-		new_hash_table->size = size;
 		new_hash_table->array = calloc(size, sizeof(hash_node_t *));
 		if (new_hash_table->array == NULL)
 		{
 			free(new_hash_table);
 			return (NULL);
 		}
+		new_hash_table->size = size;
 		return (new_hash_table);
 	}
 	return (NULL);
